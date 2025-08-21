f215f79feacf3c0dd4d65147343129f1d4f44314
Speedoxx <maximusngjp@gmail.com>
2025-08-21 23:47:07 +0800

    Buggy implementation of saved Tasks

--- src/main/java/Mael.java
+++ src/main/java/Mael.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -32,16 +36,28 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
         """;
 
     /** 
-     * Class to encapsulate Tasks and its subclasses
+     * Class to encapsulate {@code Task} and its subclasses
      */ 
     private abstract static class Task {
         private final String title;
         private boolean completed;
 
+        /**
+         * Default constructor of a {@code Task}
+         * 
+         * @param title Title of the {@code Task}
+         */
         private Task(String title) {
             this.title = title;
         }
 
+        /**
+         * Factory method for {@code Task} during user inputs 
+         * 
+         * @param text Takes user inputs with the form specified by Level-4 requirements
+         * @return Subclass of {@code Task} depending on input
+         * @throws MaelException Thrown when text input is of an unspecified form
+         */
         public static Task of(String text) throws MaelException {
             String[] sections = text.split("/");
             switch (text.split(" ")[0]) {
@@ -79,6 +95,34 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
 
         }
 
+        /**
+         * Factory method for {@code Task} during loading 
+         * 
+         * @param text Takes user inputs with the form specified in save file ../data/Mael.txt
+         * @return Subclass of {@code Task} depending on input
+         * @throws MaelException Thrown when text input is of an unspecified form
+         */
+        public static Task generate(String text) throws MaelException {
+            String[] sections = text.split(" | ");
+            switch (sections[0]) {
+                case "T":
+                    tasks.add(new ToDo(sections[2], sections[1].equals("X")));
+
+                case "D":
+                    tasks.add(new Deadline(sections[2], sections[3], sections[1].equals("X")));
+
+                case "E":
+                    tasks.add(new Event(sections[2], sections[3], sections[4], sections[1].equals("X")));
+                
+                default:
+                    throw new MaelException("Error when loading tasks...");
+            }
+        }
+        /**
+         * Marks {@code Task} as complete
+         * 
+         * @throws MaelException if {@code Task} is already complete
+         */
         public void markComplete() throws MaelException {
             if (this.completed) {
                 throw new MaelException("Mission already complete");
@@ -86,6 +130,11 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
             this.completed = true;
         }
 
+        /**
+         * Marks {@code Task} as incomplete
+         * 
+         * @throws MaelException if {@code Task} is already incomplete
+         */
         public void markIncomplete() throws MaelException {
             if (!this.completed) {
                 throw new MaelException("Mission not yet complete");
@@ -93,19 +142,56 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
             this.completed = false;
         }
 
+        /**
+         * Helper function to convert completion state to text
+         */
         private String getComplete() {
             return this.completed ? "X" : " ";
         }
+
+        /**
+         * Helper function to convert {@code Task} to text for storage
+         */
+        public abstract String saveString();
 
         @Override
         public String toString() {
             return "[" + this.getComplete() + "] " + this.title;
         }
 
+        /**
+         * Subclass that encapsulates {@code ToDo} tasks
+         */
         public static class ToDo extends Task {
 
+            /**
+             * Default constructor of a {@code ToDo} task during user input
+             * 
+             * @param title Title of the {@code ToDo}
+             * @param completed Completion state of the {@code ToDo}
+             */
+            public ToDo(String title, boolean completed) {
+                super(title);
+                if (completed) {
+                    super.completed = completed;
+                }
+            }
+
+            /**
+            * Constructor of a {@code ToDo} task during user input
+            * 
+            * @param sections String array taken from the user input, split by "/"
+            */
             public ToDo(String[] sections) {
                 super(sections[0].substring(5));
+            }
+
+            /**
+            * Helper function to convert {@code ToDo} to text for storage
+            */
+            @Override
+            public String saveString() {
+                return "T | " + super.getComplete() + " | " + super.title;
             }
 
             @Override
@@ -114,13 +200,44 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
             }
 
         }
-
+        
+        /**
+         * Subclass that encapsulates {@code Deadline} tasks
+         */
         public static class Deadline extends Task {
             private String deadline;
 
+            /**
+             * Default constructor of a {@code Deadline} task during user input
+             * 
+             * @param title Title of the {@code Deadline}
+             * @param deadline Deadline of the {@code Deadline}
+             * @param completed Completion state of the {@code Deadline}
+             */
+            public Deadline(String title, String deadline, boolean completed) {
+                super(title);
+                this.deadline = deadline;
+                if (completed) {
+                    super.completed = completed;
+                }
+            }
+
+            /**
+            * Constructor of a {@code Deadline} task during user input
+            * 
+            * @param sections String array taken from the user input, split by "/"
+            */
             public Deadline(String[] sections) {
                 super(sections[0].substring(9));
                 this.deadline = sections[1].substring(3);
+            }
+
+            /**
+            * Helper function to convert {@code Deadline} to text for storage
+            */
+            @Override
+            public String saveString() {
+                return "T | " + super.getComplete() + " | " + super.title + " | " + this.deadline;
             }
 
             @Override
@@ -130,14 +247,47 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
 
         }
 
+        /**
+         * Subclass that encapsulates {@code Event} tasks
+         */
         public static class Event extends Task {
             private String start;
             private String end;
 
+            /**
+             * Default constructor of a {@code Event} task during user input
+             * 
+             * @param title Title of the {@code Event}
+             * @param start Start Date of the {@code Event}
+             * @param end End Date the {@code Event}
+             * @param completed Completion state of the {@code Event}
+             */
+            public Event(String title, String start, String end, boolean completed) {
+                super(title);
+                this.start = start;
+                this.end = end;
+                if (completed) {
+                    super.completed = completed;
+                }
+            }
+
+            /**
+            * Constructor of a {@code Event} task during user input
+            * 
+            * @param sections String array taken from the user input, split by "/"
+            */
             public Event(String[] sections) {
                 super(sections[0].substring(6));
                 this.start = sections[1].substring(5);
                 this.end = sections[2].substring(3);
+            }
+
+            /**
+            * Helper function to convert {@code Event} to text for storage
+            */
+            @Override
+            public String saveString() {
+                return "T | " + super.getComplete() + " | " + super.title + " | " + this.start + " | " + this.end;
             }
 
             @Override
@@ -149,7 +299,7 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
     }
 
     // List of Tasks
-    private static ArrayList<Task> tasks = new ArrayList<>();
+    private static final ArrayList<Task> tasks = new ArrayList<>();
 
     /**
      * For exceptions specific to Mael
@@ -178,7 +328,7 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
 
         for (int i = 0; i < text.length; i++) {
             if (i == 4) {
-                Mael.line_by_line(LOGO);
+                Mael.lineByLine(LOGO);
             } else {
                 System.out.print(text[i]);
             }
@@ -199,7 +349,7 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
 
         for (int i = 0; i < text.length; i++) {
             if (i == 4) {
-                Mael.line_by_line(LOGO);
+                Mael.lineByLine(LOGO);
             } else {
                 System.out.print(text[i]);
             }
@@ -226,7 +376,7 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
      * 
      * @param text Text to be displayed line by line
      */
-    private static void line_by_line(String text) throws InterruptedException {
+    private static void lineByLine(String text) throws InterruptedException {
         String[] lines = text.split("\n");
         for (String line : lines) {
             System.out.println(line);
@@ -236,7 +386,34 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
         }
     }
     public static void main(String[] args) throws InterruptedException {
-        launch();
+        //launch();
+        try {
+            File taskFolder = new File("../data");
+            if (!taskFolder.exists()) {
+                taskFolder.mkdir();
+            } else if (taskFolder.isFile()) {
+                taskFolder.delete();
+                taskFolder.mkdir();
+            }
+            File taskFile = new File("../data/Mael.txt");
+            if (!taskFile.exists()) {
+                taskFile.createNewFile();
+            } else if (taskFile.isDirectory()) {
+                taskFile.delete();
+                taskFile.createNewFile();
+            }
+            Scanner taskReader = new Scanner(taskFile);
+            while (taskReader.hasNextLine()) {
+                String currLine = taskReader.nextLine();
+                tasks.add(Task.generate(currLine));
+            }
+            taskReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | MaelException e) {
+            e.printStackTrace();
+        }
+
         String input = SCANNER.nextLine();
         while (!input.equals("bye")) {  // Early termination when "bye" is input
             line();
@@ -254,9 +431,9 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
                 case "mark", "m" -> {
                     if (input.split(" ").length == 2) {
                         try {
-                            int task_num = Integer.parseInt(input.split(" ")[1]);
-                            tasks.get(task_num - 1).markComplete();
-                            System.out.println("\t" + tasks.get(task_num - 1));
+                            int taskNum = Integer.parseInt(input.split(" ")[1]);
+                            tasks.get(taskNum - 1).markComplete();
+                            System.out.println("\t" + tasks.get(taskNum - 1));
                             System.out.println("\t\t-Mission Completed-");
                         } catch (NumberFormatException e) {
                             System.out.println(new MaelException("Mark details unclear"));
@@ -272,9 +449,9 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
                 case "unmark", "um" -> {
                     if (input.split(" ").length == 2) {
                         try {
-                            int task_num = Integer.parseInt(input.split(" ")[1]);
-                            tasks.get(task_num - 1).markIncomplete();
-                            System.out.println("\t" + tasks.get(task_num - 1));
+                            int taskNum = Integer.parseInt(input.split(" ")[1]);
+                            tasks.get(taskNum - 1).markIncomplete();
+                            System.out.println("\t" + tasks.get(taskNum - 1));
                             System.out.println("\t\t-Mission Unsuccessful-");
                         } catch (NumberFormatException e) {
                             System.out.println(new MaelException("Unmark details unclear"));
@@ -290,9 +467,9 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
                 case "delete", "del" -> {
                     if (input.split(" ").length == 2) {
                         try {
-                            int task_num = Integer.parseInt(input.split(" ")[1]);
-                            System.out.println("\t" + tasks.get(task_num - 1));
-                            tasks.remove(task_num - 1);
+                            int taskNum = Integer.parseInt(input.split(" ")[1]);
+                            System.out.println("\t" + tasks.get(taskNum - 1));
+                            tasks.remove(taskNum - 1);
                             System.out.println("\t\t-Mission Terminated-");
                         } catch (NumberFormatException e) {
                             System.out.println(new MaelException("Termination details unclear"));
@@ -316,6 +493,16 @@ dXXXXXXXXXXXb   d|b   dXXXXXXXXXXXb
             line();
             input = SCANNER.nextLine();
         }
-        close();
+
+        try {
+            FileWriter taskWriter = new FileWriter("../data/Mael.txt");
+            for (Task task : tasks) {
+                taskWriter.write(task.saveString());
+            }
+            taskWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //close();
     }
 }
