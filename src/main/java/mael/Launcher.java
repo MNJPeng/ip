cd199bc73b5b9b7291c259989e8f7ffa6c6e896e
Speedoxx <maximusngjp@gmail.com>
2025-09-04 23:25:11 +0800

    Add DialogBox

---
+++ src/main/java/mael/Launcher.java
@@ -0,0 +1,10 @@
+package mael;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Mael.class, args);
+    }
+    
+}
